204f3e8b16f08f87bc10d5b25da628261a21f8fd
thenameisrohit <127139000+thenameisrohit@users.noreply.github.com>
2024-04-18 23:58:46 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer style={{ marginTop: "150px", textAlign: "center" }}>
+      <p style={{ color: "white" }}>
+        &copy; 2024 Profile Explorer. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
